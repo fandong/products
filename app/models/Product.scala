d15825a9a410ccde17c70311d8a8cbba50fa2677
fdong <dwest2000@hotmail.com>
2013-10-22 23:46:25 -0400

    product

---
+++ app/models/Product.scala
@@ -0,0 +1,25 @@
+package models
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Fan
+ * Date: 22/10/13
+ * Time: 10:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+case class Product (ean: Long, name: String, description: String)
+
+object Product {
+
+  var products = Set (
+    Product(5010, "Paperclips", "Large plain pack of 100"),
+    Product(5011, "Giant Paperclips", "Giant plain pack of 100"),
+    Product(5012, "Zebra Paperclips", "Zebra length plain pack of 100")
+  )
+
+  def findAll() = this.products.toList.sortBy(_.ean)
+}
+
+
+
+
